910c87adfc4322d85d23e53b0336f9684477cfd7
RJ Salicco <rj.salicco@axiomaticit.com>
2018-02-04 17:48:48 +0000

    Added more validation and updated error messages.

--- src/main/java/com/axiomaticit/validator/ItemValidator.java
+++ src/main/java/com/axiomaticit/validator/ItemValidator.java
@@ -15,12 +15,18 @@ public class ItemValidator implements Validator {
 		Item item = (Item) target;
 		
 		if(item.getAuthor() == null || item.getAuthor().length() < 1)
-			errors.rejectValue("author", "item.author.error");
+			errors.rejectValue("author", "item.author.empty.error");
+		
+		if(item.getContent() == null || item.getContent().length() < 1)
+			errors.rejectValue("content", "item.content.empty.error");
+			
+		if(item.getContent() == null || item.getContent().length() < 10)
+			errors.rejectValue("content", "item.content.min.error");
+			
+		if(item.getContent() == null || item.getContent().length() > 50)
+			errors.rejectValue("content", "item.content.max.error");
 		
 		if(item.getTitle() == null || item.getTitle().length() < 1)
-			errors.rejectValue("title", "item.title.error");
-		
-		if(item.getContent() == null || item.getContent().length() < 10)
-			errors.rejectValue("content", "item.content.error");
+			errors.rejectValue("title", "item.title.empty.error");
 	}
 }
