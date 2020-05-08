33c7249bd758de7ce689d22f5ca335f1fe24caee
R.J. Salicco <rj.salicco@axiomaticit.com>
2020-05-08 01:38:52 +0000

    Updated XML element namespace

--- src/main/java/com/axiomaticit/model/Item.java
+++ src/main/java/com/axiomaticit/model/Item.java
@@ -15,61 +15,61 @@ import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name="items")
-@XmlRootElement(name="item", namespace="org.axiomaticit.model")
+@XmlRootElement(name="item", namespace="com.axiomaticit.model")
 public class Item extends Model implements Serializable {
 
 	private static final long serialVersionUID = 8986221083999860039L;
-	
+
 	@Id
 	@GeneratedValue(generator="system-uuid")
 	@GenericGenerator(name="system-uuid", strategy = "uuid")
 	private String id;
-	
+
 	@Column(name="title")
 	private String title;
-	
+
 	@Column(name="content")
 	private String content;
-	
+
 	@Column(name="author")
 	private String author;
-	
+
 	public Item () { }
-	
+
 	public Item(String title, String content, String author) {
 		this.title = title;
 		this.content = content;
 		this.author = author;
 	}
-	
+
 	public String getId() {
 		return this.id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	public String getTitle() {
 		return this.title;
 	}
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
 	public String getContent() {
 		return this.content;
 	}
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
+
 	public String getAuthor() {
 		return this.author;
 	}
 	public void setAuthor(String author) {
 		this.author = author;
 	}
-	
+
 	public boolean equals(Object obj) {
 		if(obj  == null)
 			return false;
@@ -77,10 +77,10 @@ public class Item extends Model implements Serializable {
 			return true;
 		if(obj instanceof Item)
 			return equals((Item) obj);
-		
+
 		return false;
 	}
-	
+
 	public boolean equals(Item item) {
 		return new EqualsBuilder().
 				append(id, item.getId()).
@@ -88,7 +88,7 @@ public class Item extends Model implements Serializable {
 				append(title, item.getTitle()).
 				isEquals();
 	}
-	
+
 	public int hashCode() {
 		return new HashCodeBuilder(3,5).
 				append(id).
